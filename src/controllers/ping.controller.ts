0b5872281892f69cf94be87b7b7f0468a0d5a76c
Nguyễn Trường Lâm <lam.nguyen@wecheer.io>
2023-03-11 12:17:00 +0700

    add journeys

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -1,4 +1,4 @@
-import { inject } from '@loopback/core';
+import { inject } from "@loopback/core";
 import {
   Request,
   RestBindings,
@@ -6,29 +6,29 @@ import {
   response,
   ResponseObject,
   param,
-} from '@loopback/rest';
-import { type } from 'os';
-import { ClevertabService } from '../services';
-import data from './dataaaa.json'
+} from "@loopback/rest";
+import { type } from "os";
+import { ClevertabService } from "../services";
+import data from "./dataaaa.json";
 
 /**
  * OpenAPI response for ping()
  */
 const PING_RESPONSE: ResponseObject = {
-  description: 'Ping Response',
+  description: "Ping Response",
   content: {
-    'application/json': {
+    "application/json": {
       schema: {
-        type: 'object',
-        title: 'PingResponse',
+        type: "object",
+        title: "PingResponse",
         properties: {
-          greeting: { type: 'string' },
-          date: { type: 'string' },
-          url: { type: 'string' },
+          greeting: { type: "string" },
+          date: { type: "string" },
+          url: { type: "string" },
           headers: {
-            type: 'object',
+            type: "object",
             properties: {
-              'Content-Type': { type: 'string' },
+              "Content-Type": { type: "string" },
             },
             additionalProperties: true,
           },
@@ -44,8 +44,9 @@ const PING_RESPONSE: ResponseObject = {
 export class PingController {
   constructor(
     @inject(RestBindings.Http.REQUEST) private req: Request,
-    @inject('services.ClevertabService') protected clevertabService: ClevertabService,
-  ) { }
+    @inject("services.ClevertabService")
+    protected clevertabService: ClevertabService
+  ) {}
 
   // Map to `GET /ping`
   // @get('/ping')
@@ -60,72 +61,220 @@ export class PingController {
   //   };
   // }
 
-  @get('/download')
+  @get("/download-campaigns")
   @response(200, PING_RESPONSE)
   async find(
-    @param.query.string('from') from: string,
-    @param.query.string('to') to: string,
-    @param.query.string('param') param: string,): Promise<any> {
-    let split: any = param.split('-H ')
-    let cookie = ''
-    let csrf = ''
+    @param.query.string("from") from: string,
+    @param.query.string("to") to: string,
+    @param.query.string("param") param: string
+  ): Promise<any> {
+    let split: any = param.split("-H ");
+    let cookie = "";
+    let csrf = "";
     for (const iterator of split) {
       if (iterator.toString().search("cookie") == 1) {
-        cookie = iterator.toString().substr(9, iterator.toString().length - 15)
+        cookie = iterator.toString().substr(9, iterator.toString().length - 15);
       }
       if (iterator.toString().search("x-clevertap-csrf-token") == 1) {
-        csrf = iterator.toString().substr(25, iterator.toString().length - 43)
+        csrf = iterator.toString().substr(25, iterator.toString().length - 43);
       }
     }
+    let lst: any = await this.clevertabService.GetListCampaign(csrf, cookie);
 
-    let lst: any = await this.clevertabService.GetListCampaign(csrf, cookie)
-    let arr: any = []
-    let count = 0
+    let arr: any = [];
+    let count = 0;
     for (const iterator of JSON.parse(lst.body).targets) {
-      let id = iterator._id
-      
-      let name = iterator.name
-      console.log('process... ' + count + '/' + JSON.parse(lst.body).targets.length);
+      let id = iterator._id;
+
+      let name = iterator.name;
+      console.log(
+        "process... " + count + "/" + JSON.parse(lst.body).targets.length
+      );
       console.log(name);
       count++;
-      let details: any = await this.clevertabService.GetDetailCampaign(csrf, cookie, id, from, to);
+      let details: any = await this.clevertabService.GetDetailCampaign(
+        csrf,
+        cookie,
+        id,
+        from,
+        to
+      );
       if (details.body != '{ "All" : { }}') {
-        let obj: any = []
-        let split1: any = JSON.stringify(JSON.parse(details.body).All).split('"2')
+        let obj: any = [];
+        let split1: any = JSON.stringify(JSON.parse(details.body).All).split(
+          '"2'
+        );
         for (const iterator of split1) {
-          if (iterator != '{') {
-            let split2: any = iterator.split('":{')
-            let sent = JSON.parse(('{' + split2[1]).replace('}}', '}').replace('},', '}'))
+          if (iterator != "{") {
+            let split2: any = iterator.split('":{');
+            let sent = JSON.parse(
+              ("{" + split2[1]).replace("}}", "}").replace("},", "}")
+            );
             arr.push({
-              date: '2' + split2[0],
+              date: "2" + split2[0],
               sent: sent.sent,
               id: id,
               name: name,
-              error: sent.errors
-            })
+              error: sent.errors,
+            });
             console.log(new Date().getTime());
           }
         }
       } else {
         arr.push({
-          date: '0',
-          sent: '0',
+          date: "0",
+          sent: "0",
           id: id,
           name: name,
-          error: ''
-        })
+          error: "",
+        });
       }
     }
-    
+
     var XLSX = require("xlsx");
     const date = new Date();
-    const nameGenerate = 'FileExport_Campaign_' + Date.parse(date.toString()) + '.xlsx';
+    const nameGenerate =
+      "FileExport_Campaign_" + Date.parse(date.toString()) + ".xlsx";
     let fileName = `public/` + nameGenerate;
     const ws: any = XLSX.utils.json_to_sheet(arr);
     const wb: any = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, 'Data');
+    XLSX.utils.book_append_sheet(wb, ws, "Data");
     XLSX.writeFile(wb, fileName);
-    console.log('Done !!!');
-    return {Status: 'Successfuly !!!'};
+    console.log("Done !!!");
+    return { Status: "Successfuly !!!" };
+  }
+
+  @get("/download-journeys")
+  @response(200, PING_RESPONSE)
+  async getJourney(
+    @param.query.string("from") from: string,
+    @param.query.string("to") to: string,
+    @param.query.string("param") param: string
+  ): Promise<any> {
+    let split: any = param.split("-H ");
+    let cookie = "";
+    let csrf = "";
+    for (const iterator of split) {
+      if (iterator.toString().search("cookie") == 1) {
+        cookie = iterator.toString().substr(9, iterator.toString().length - 15);
+      }
+      if (iterator.toString().search("x-clevertap-csrf-token") == 1) {
+        csrf = iterator.toString().substr(25, iterator.toString().length - 43);
+      }
+    }
+
+    let lstJourneys: any = await this.clevertabService.GetListJourneys(
+      csrf,
+      cookie
+    );
+    // let itemJourney: any = await this.clevertabService.GetIDJourneys(csrf, cookie, 85, from, to)
+    // let nodeStats: any = await this.clevertabService.GetNodeStats(csrf, cookie, 25021, from, to)
+
+    let arr: any = [];
+    let arrNodeID: any = [];
+    let count = 0;
+    for (const iterator of JSON.parse(lstJourneys.body).targets) {
+      let id = iterator._id;
+      let name = iterator.name;
+      console.log(
+        "process... " +
+          count +
+          "/" +
+          JSON.parse(lstJourneys.body).targets.length
+      );
+      console.log(name);
+      count++;
+
+      let itemJourney: any = await this.clevertabService.GetIDJourneys(
+        csrf,
+        cookie,
+        id,
+        from,
+        to
+      );
+      let object = JSON.parse(itemJourney.body).root[0];
+      for (const key in object) {
+        if (Object.prototype.hasOwnProperty.call(object, key)) {
+          if (key == "path") {
+            const element = object[key];
+            for (const itemKey in element) {
+              let key = element[itemKey].key;
+              if (key.substr(0, 7) == "message") {
+                const _id = element[itemKey].steps[0]._id;
+                arrNodeID.push({
+                  journeyID: id,
+                  name: name,
+                  nodeStatID: _id,
+                  type: itemKey,
+                });
+                console.log(JSON.stringify({
+                  journeyID: id,
+                  name: name,
+                  nodeStatID: _id,
+                  type: itemKey,
+                }));
+              }
+            }
+          }
+        }
+      }
+    }
+
+    for (const iterator of arrNodeID) {
+      let nodeStats: any = await this.clevertabService.GetNodeStats(
+        csrf,
+        cookie,
+        iterator.nodeStatID,
+        from,
+        to
+      );
+      let type;
+      if (iterator.type.search("message_email") != -1) {
+        type = "Email";
+      } else if (iterator.type.search("message_sms") != -1) {
+        type = "SMS";
+      }
+
+      if (nodeStats.body != '{ "All" : { }}') {
+        let obj = JSON.parse(nodeStats.body)["All"];
+        for (const key in obj) {
+          const element = obj[key];
+          arr.push({
+            JOURNEYID: iterator.journeyID,
+            JOURNEYNAME: iterator.name,
+            TYPE: type,
+            DATE: key,
+            SENT: element.sent,
+            VIEWED: element.viewed,
+            ERRORS: element.errors,
+          });
+          console.log('Array existing: ' + arr.length);
+        }
+      } else {
+        arr.push({
+          JOURNEYID: iterator.journeyID,
+          JOURNEYNAME: iterator.name,
+          TYPE: type,
+          DATE: "-",
+          SENT: 0,
+          VIEWED: 0,
+          ERRORS: 0,
+        });
+        console.log('Array existing: ' + arr.length);
+        
+      }
+    }
+
+    var XLSX = require("xlsx");
+    const date = new Date();
+    const nameGenerate =
+      "FileExport_Journeys_" + Date.parse(date.toString()) + ".xlsx";
+    let fileName = `public/` + nameGenerate;
+    const ws: any = XLSX.utils.json_to_sheet(arr);
+    const wb: any = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, "Data");
+    XLSX.writeFile(wb, fileName);
+    console.log("Done !!!");
+    return { Status: "Successfuly !!!" };
   }
 }
--- src/services/clevertab.service.ts
+++ src/services/clevertab.service.ts
@@ -94,4 +94,138 @@ export class ClevertabService {
       return null;
     }
   }
+
+  async GetListJourneys(csrf: any, cookie: any){
+    const request = require('request');
+    const fs = require('fs');
+    try {
+      return await new Promise(function (resolve: any, reject: any) {
+        const options = {
+          method: 'GET',
+          url: 'https://eu1.dashboard.clevertap.com/44K-948-K85Z/json/targeting/journeys/load?q={"pageNumber":1,"prefiltered":null,"searchKeyword":null,"sortField":"updated_ts","sortOrder":-1,"stc":1,"dateFrom":"20230223","dateTo":"20230325","pageSize":80}&uc=1&requestTs=1678461166825',
+          headers: {
+            'authority': 'eu1.dashboard.clevertap.com',
+          'accept': 'application/json, text/plain, */*',
+          'accept-language': 'en-US,en;q=0.9,vi-VN;q=0.8,vi;q=0.7,fr-FR;q=0.6,fr;q=0.5',
+          'cookie': cookie,
+          'referer': 'https://eu1.dashboard.clevertap.com/44K-948-K85Z/journeys/',
+          'sec-ch-ua': '"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"macOS"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-origin',
+          'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+          'x-clevertap-csrf-token': csrf
+          },
+          form: {
+            'id': '1663745095',
+            'from': '20220928',
+            'to': '20221028'
+          },
+        };
+        request(options, function (error: any, response: any) {
+          if (error) reject(error);
+          try {
+            resolve(response);
+          } catch (error_) {
+            reject(error_);
+          }
+        });
+      });
+    } catch (error) {
+      return null;
+    }
+  }
+
+  async GetIDJourneys(csrf: any, cookie: any, id: any, from: any, to: any){
+    const request = require('request');
+    const fs = require('fs');
+    try {
+      return await new Promise(function (resolve: any, reject: any) {
+        const options = {
+          method: 'POST',
+          url: 'https://eu1.dashboard.clevertap.com/44K-948-K85Z/json/interact/journeys/stats?id='+id+'&from='+from+'&to='+to+'&uc=1&requestTs=1678502427598',
+          headers: {
+            'authority': 'eu1.dashboard.clevertap.com',
+            'accept': 'application/json, text/plain, */*',
+            'accept-language': 'en-US,en;q=0.9,vi-VN;q=0.8,vi;q=0.7,fr-FR;q=0.6,fr;q=0.5',
+            'content-type': 'application/x-www-form-urlencoded',
+            'cookie': cookie,
+            'origin': 'https://eu1.dashboard.clevertap.com',
+            'referer': 'https://eu1.dashboard.clevertap.com/44K-948-K85Z/journeys/journey/'+id+'/report/node-stats',
+            'sec-ch-ua': '"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"macOS"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+            'x-clevertap-csrf-token': csrf
+          },
+          form: {
+            "journeyId": id,
+            "fromDate": from,
+            "toDate": to
+          },
+        };
+        request(options, function (error: any, response: any) {
+          if (error) reject(error);
+          try {
+            resolve(response);
+          } catch (error_) {
+            reject(error_);
+          }
+        });
+      });
+    } catch (error) {
+      return null;
+    }
+  }
+
+
+  async GetNodeStats(csrf: any, cookie: any, id: any, from: any, to: any){
+    const request = require('request');
+    const fs = require('fs');
+    try {
+      return await new Promise(function (resolve: any, reject: any) {
+        const options = {
+          method: 'POST',
+          url: 'https://eu1.dashboard.clevertap.com/44K-948-K85Z/json/notification/calculateTrend.html?uc=1&requestTs=1678446067519',
+          headers: {
+            'authority': 'eu1.dashboard.clevertap.com',
+            'accept': 'application/json, text/plain, */*',
+            'accept-language': 'en-US,en;q=0.9,vi-VN;q=0.8,vi;q=0.7,fr-FR;q=0.6,fr;q=0.5',
+            'content-type': 'application/x-www-form-urlencoded',
+            'cookie': cookie,
+            'origin': 'https://eu1.dashboard.clevertap.com',
+            'referer': 'https://eu1.dashboard.clevertap.com/44K-948-K85Z/campaigns/campaign/1663745095/report/stats/trend',
+            'sec-ch-ua': '"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"macOS"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+            'x-clevertap-csrf-token': csrf
+          },
+          form: {
+            "id": id,
+            "from": from,
+            "to": to
+          },
+        };
+        request(options, function (error: any, response: any) {
+          if (error) reject(error);
+          try {
+            resolve(response);
+          } catch (error_) {
+            reject(error_);
+          }
+        });
+      });
+    } catch (error) {
+      return null;
+    }
+  }
 }
